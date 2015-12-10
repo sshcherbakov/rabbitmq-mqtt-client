3891320dce1abd61af9362d78d136a1b27a8540a
sshcherbakov <sshcherbakov@pivotal.io>
2015-12-10 10:55:48 +0100

    Initial commit

---
+++ src/main/java/com/demo/rabbitmq/mqtt/RabbitmqMqttClientApplication.java
@@ -0,0 +1,12 @@
+package com.demo.rabbitmq.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqMqttClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqMqttClientApplication.class, args);
+    }
+}
---
+++ src/test/java/com/demo/rabbitmq/mqtt/RabbitmqMqttClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.rabbitmq.mqtt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RabbitmqMqttClientApplication.class)
+public class RabbitmqMqttClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
